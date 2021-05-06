38c49274beaeff75750212c77bd5b8aa648ba36c
Torrekie <torrekie@TorrekiedeMacBook-Pro.local>
2021-05-07 02:25:34 +0800

    buggy codes

--- main.c
+++ main.c
@@ -26,6 +26,18 @@ static const bool UNDER_DEVELOPMENT = true; //When this set true, function print
 static const bool BETA_RELEASE = false;     //When this set true, function printVeriosn will output Beta Version.
 static const bool COMPLETED = false;        //When this set false, you shouldn't build WillDaemon for use, because it means we didn't finish the basic features of this project so you can't really get it in use.
 
+static const char *PROGRAM_NAME;
+
+int printUsage(void)
+{
+  printf("usage: %s [arguments]\r\n", PROGRAM_NAME);
+  printf("arguments:\r\n");
+  printf("\t-h or --help \tPrint manuals (this message)\r\n");
+  printf("\t-v or --version \tPrint version\r\n");
+  printf("\t-f or --full \tPrint full info (use with -v)\r\n");
+  return 0;
+}
+
 int printVersion(bool simple)
 {
   char versionOutput[100];
@@ -44,25 +56,30 @@ int printVersion(bool simple)
   }
   else
   {
-    printf("WillDaemon %s\nOriginally created by Torrekie, maintained by her friends.\n", versionOutput);
+    printf("WillDaemon %s%s\nOriginally created by Torrekie, maintained by her friends.\n", versionOutput, (!BETA_RELEASE && COMPLETED) ? " stable" : " unstable");
   }
   return 0;
 }
 int main(int argc, char **argv)
 {
+  PROGRAM_NAME = argv[0];
   if (argc < 2)
   {
-    fprintf(stderr, "WillDaemon main project, unfinished.\n\033[0;37mType \'%s --help\' to see the instructions.\033[0;0m\n", argv[0]);
+    fprintf(stderr, "WillDaemon main project, unfinished.\n\033[0;37mType \'%s --help\' to see the instructions.\033[0;0m\n", PROGRAM_NAME);
     exit(1);
   }
   else
   {
     int opt;
-    bool showSimple = false;
+
+    bool showSimple = true;
+    bool versionRequest = false;
+    bool usageRequest = false;
+
     const char *short_options = "a:hvf";
     int option_index = 0;
     struct option long_options[] = {
-        {"add", required_argument, 0, 'a'},
+        //{"add", required_argument, 0, 'a'},
         {"help", no_argument, 0, 'h'},
         {"version", no_argument, 0, 'v'},
         {"full", no_argument, 0, 'f'},
@@ -71,28 +88,42 @@ int main(int argc, char **argv)
     {
       switch (opt)
       {
-      case 'a':
-        printf("option: -%c, optarg: %s\r\n", opt, optarg);
-        break;
+      //case 'a':
+      //printf("option: -%c, optarg: %s\r\n", opt, optarg);
+      //break;
       case 'h':
-        //printUsage();
+        usageRequest = true;
         break;
       case 'f':
-        showSimple = true;
-        //printf("%c",opt);
+        showSimple = false;
         break;
       case 'v':
-        printVersion(showSimple);
+        versionRequest = true;
         break;
-      case '?': 
+      case '?':
         printf("Argument error.\r\n");
-        printf("Type \'%s --help\' to see the instructions.\033[0;0m\n", argv[0]);
+        printf("Type \'%s --help\' to see the instructions.\033[0;0m\n", PROGRAM_NAME);
         break;
       default:
-        printf("%s: invalid option -- '%c'\r\n", argv[0], opt);
-        printf("Type \'%s --help\' to see the instructions.\033[0;0m\n", argv[0]);
-        break;
+        printf("%s: invalid option -- '%c'\r\n", PROGRAM_NAME, opt);
+        printf("Type \'%s --help\' to see the instructions.\033[0;0m\n", PROGRAM_NAME);
       }
+    }
+    //Terrible code starts now, remember to close your eyes.
+    if (versionRequest && !usageRequest)
+    {
+      if (printVersion(showSimple) != 0)
+        printf("What happend with my version info? I cannot print it out.\n");
+    }
+    else if (!showSimple)
+    {
+      printf("Argument error: --full should always use with --version.\r\n");
+      printf("Type \'%s --help\' to see the instructions.\033[0;0m\n", PROGRAM_NAME);
+    }
+    else if (!versionRequest && usageRequest)
+    {
+      if (printUsage() != 0)
+        printf("What happend with my manual? I cannot print it out.\n");
     }
     return 0;
   }
