7d4304e538dbb79efa8afde4d55d75808a3070d6
lampese@github.com <1523284180@qq.com>
2021-08-03 17:15:42 +0800

    Update fuction about config

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,6 @@
 #include "lib/Config.h"
 #include "lib/Opt.h"
 #include "lib/OS.h"
-#include "lib/jsonxx/json.hpp"
-using namespace std;
 int main(int argc,char *argv[]){
     vector<string>str={
 		"--help","-h",
@@ -16,5 +14,23 @@ int main(int argc,char *argv[]){
     for(int i=0;i<Ret.size();++i){
         if(Ret[i].CommandName=="-h")PrintVersion(),PrintArguments();
         else if(Ret[i].CommandName=="-v")PrintVersion();
+        else if(Ret[i].CommandName=="-r")ClearCache();
+        else if(Ret[i].CommandName=="-c"){
+            if(AuthFile("config.json")){
+                printf("There is already a config in this directory, do you want to continue (Y/n)?");
+                char option=getchar();
+                while(option!='y'&&option!='Y'&&option!='n'&&option!='N'&&option!='\n')option=getchar();
+                if(option=='n'||option=='N'){
+                    puts("Give up creating configuration files.");
+                    return 0;
+                }else{
+                    ClearCache();
+                    CreateConfig();
+                }
+            }
+        }else if(Ret[i].CommandName=="-s"){
+            if(!AuthFile("config.json"))throw WDexception("Error:config not found, try -c?");
+            CheckConfig();
+        }
     }
 }
--- lib/OS.h
+++ lib/OS.h
@@ -6,6 +6,8 @@
 #include<limits.h>
 #include<fstream>
 #include<string>
+#include "jsonxx/json.hpp"
+using namespace std;
 class WDexception:public std::exception{
 	public:
 		std::string error;
@@ -16,7 +18,56 @@ class WDexception:public std::exception{
 			return error.c_str();
 		}
 };
-inline bool AuthFile(const std::string& name) {
+inline string GetAllData(char *FileName){
+	ifstream JsonIn(FileName); 
+	string result((istreambuf_iterator<char>(JsonIn)),istreambuf_iterator<char>());
+	return result;
+}
+inline bool AuthFile(const std::string& name){
     ifstream f(name.c_str());
     return f.good();
+}
+void ClearCache(){
+	if(AuthFile("config.json"))remove("config.json");
+    if(AuthFile(".daemonize_history"))remove(".daemonize_history");
+    puts("Cleared.");
+}
+void CreateConfig(){
+	jsonxx::json Config={
+		"Github",{
+			{"User",""},
+			{"Repo",""},
+			{"Token",""}
+		},
+		"Limit",{
+			{"Time",""},
+			{"Sign",""}
+		},
+		"Web",{
+			{"Open",true},
+			{"Port",80},
+		},
+		"Cli",{
+			{"Open",true},
+			{"Port",16384}
+		}
+	};
+}
+bool CheckConfig(){ 
+	string ConfigString=GetAllData("config.json");
+	jsonxx::json Config=jsonxx::json::parse(ConfigString);
+	if(Config["Github"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Github\"");
+	if(Config["Github"]["User"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Github.User\"");
+	if(Config["Github"]["Repo"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Github.Repo\"");
+	if(Config["Github"]["Token"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Github.Token\"");
+	if(Config["Limit"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Limit\"");
+	if(Config["Limit"]["Time"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Limit.Time\"");
+	if(Config["Limit"]["Sign"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Limit.Sign\"");
+	if(Config["Web"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Web\"");
+	if(Config["Web"]["Open"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Web.Open\"");
+	if(Config["Web"]["Port"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Web.Port\"");
+	if(Config["Cli"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Cli\"");
+	if(Config["Cli"]["Open"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Cli.Open\"");
+	if(Config["Cli"]["Port"]==nullptr)throw WDexception("Error:Config doesn't hava key \"Cli.Port\"");
+	return true;
 }
